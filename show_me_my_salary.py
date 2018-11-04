aa159c2a2b8dd6e0295b44562bf2a5eb7beb116b
Seungmin.Ryu <yakkle@gmail.com>
2018-11-05 01:10:18 +0900

    add GUI by pyside2

--- show_me_my_salary.py
+++ show_me_my_salary.py
@@ -1,32 +1,9 @@
 import click
 import platform
 import subprocess
-import pdfkit
 
 from os import path
-from utils import get_parsed_data, get_encrypted, get_decrypted
-
-
-def convert_html(decrypted_html, new_html_file):
-    with open(new_html_file, 'w', encoding='utf-8') as f:
-        f.write(decrypted_html)
-
-    print(f"'{new_html_file}' was created.")
-
-
-def convert_pdf(decrypted_html, new_pdf_file):
-    pdfkit.from_string(decrypted_html, new_pdf_file)
-
-
-def decrypt_html(file_path, password):
-    with open(file_path, encoding='utf-8') as f:
-        data = ''.join(f.readlines())
-        title, data = get_parsed_data(data)
-        iv, salt, encrypted = get_encrypted(data)
-
-    decrypted = get_decrypted(encrypted, iv, salt, password)
-
-    return title, decrypted
+from converter import decrypt_html, convert_html, convert_pdf
 
 
 @click.command('salary')
